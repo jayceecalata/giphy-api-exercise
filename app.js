9e347a3228f0b80a19c9035914dce3d44726efaa
jayceecalata <132433212+jayceecalata@users.noreply.github.com>
2024-05-06 14:42:40 +0800

    feat: implement handling possible use-cases
    - prevent an unwanted search when searchbar is cleared
    - display an image placeholder on window load
    - display a gif when a search is being processed
    - display a gif when an error is encountered

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const searchBar = document.getElementById('js-search');
 let userInput;
 
 searchBar.onsearch = () => { 
-    userInput = searchBar.value;
-    searchGif(userInput);
+    // prevent whitespace/s & unwanted search
+    if (searchBar.value.trim()) { 
+        userInput = searchBar.value;
+        searchGif(userInput);
+        displayLoading();
+    }
 }
 
 const endPoint = 'https://api.giphy.com/v1/gifs/translate';
@@ -14,5 +18,21 @@ const apiKey = 'Y2AHOQRuoZGY2cWPNRDIPwIPq75uWmSc';
 async function searchGif(userInput) {
     const gif = await fetch(`${endPoint}?api_key=${apiKey}&s=${userInput}`, {mode: 'cors'});
     const json = await gif.json();
-    img.src = json.data.images.original.url;
+    try {
+        img.src = json.data.images.original.url;
+    } catch (error) {
+        const errorImg = './assets/something-went-wrong.gif';
+        console.log(error);
+        img.src = errorImg;
+    }
+}
+
+const loadingGif = './assets/loading.gif';
+function displayLoading() {
+    img.src = loadingGif;
+}
+
+const imgPlaceholder = './assets/placeholder.png';
+window.onload = () => {
+    img.src = imgPlaceholder;
 }
